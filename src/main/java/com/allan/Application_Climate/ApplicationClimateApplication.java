9550e22ce51fe1e7a56e68fa1544645ed0cedff4
Allan <allanvictorsilvaandrade@gmail.com>
2023-09-06 23:00:33 -0300

    Commit inicial

---
+++ src/main/java/com/allan/Application_Climate/ApplicationClimateApplication.java
@@ -0,0 +1,13 @@
+package com.allan.Application_Climate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationClimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationClimateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allan/Application_Climate/ApplicationClimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allan.Application_Climate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationClimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
